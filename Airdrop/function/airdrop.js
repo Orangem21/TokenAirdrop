500722be96fa15635d96a11b56c646f08baa6321
OrangeM21 <orangemtony@gmail.com>
2018-02-28 01:59:33 +0800

    add

--- Airdrop/function/airdrop.js
+++ Airdrop/function/airdrop.js
@@ -3,7 +3,7 @@
  */
 
 const Config = require('./../config/config.js');
-var sleep = require('sleep');
+//var sleep = require('sleep');
 Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider(Config.transaction.url));
 
